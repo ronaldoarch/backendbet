e437fbf92303659fa127d7594fa8a7a9b542f1fe
Ronaldo dias de sousa <ronaldohunter54@gmail.com>
2025-12-22 15:12:01 -0300

    Atualizar authRoutes para usar authController

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,15 +1,12 @@
 import express from 'express'
 import * as authController from '../controllers/authController.js'
-import { authenticateToken } from '../middleware/auth.js'
 
 const router = express.Router()
 
+// Rotas de autenticação
 router.post('/login', authController.login)
 router.post('/register', authController.register)
-router.get('/verify', authenticateToken, authController.verify)
-router.post('/me', authenticateToken, authController.me)
-router.post('/logout', authenticateToken, authController.logout)
+router.post('/logout', authController.logout)
 
 export default router
-
 
