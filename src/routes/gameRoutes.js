69622b985cd4d609a8c36a0ae515bed50c7c0d20
Ronaldo dias de sousa <ronaldohunter54@gmail.com>
2025-12-30 17:28:42 -0300

    Corrigir: adicionar playfiver.js ao Git e atualizar gameRoutes.js

--- src/routes/gameRoutes.js
+++ src/routes/gameRoutes.js
@@ -1,20 +1,20 @@
 import express from 'express'
 import * as gameController from '../controllers/gameController.js'
-import { authenticateToken, optionalAuth } from '../middleware/auth.js'
+import { authenticateToken } from '../middleware/auth.js'
 
 const router = express.Router()
 
-// Rotas diretas
+// Rotas públicas de jogos
 router.get('/all', gameController.getAllGames)
+router.get('/featured', gameController.getFeaturedGames)
+router.get('/source', gameController.getSourceGames)
+router.get('/casinos', gameController.getCasinoGames)
+
+// Rotas autenticadas de jogos
 router.get('/single/:id', authenticateToken, gameController.getSingleGame)
 router.get('/favorites', authenticateToken, gameController.getFavorites)
 router.post('/favorite/:id', authenticateToken, gameController.toggleFavorite)
 router.post('/like/:id', authenticateToken, gameController.toggleLike)
-
-// Rotas com prefixos (usadas com router.use('/featured', ...), etc)
-router.get('/games', gameController.getFeaturedGames) // Para /featured/games
-router.get('/games', optionalAuth, gameController.getCasinoGames) // Para /casinos/games  
-router.get('/games', gameController.getSourceGames) // Para /source/games
 
 export default router
 
